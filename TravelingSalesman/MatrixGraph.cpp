f6b751adb9c5f461a2009eea63c05293613e634b
Matthew Dwyer <dwyermw@muohio.edu>
2012-11-30 23:13:21 -0500

    Added method stubs in MatrixGraph.cpp

--- TravelingSalesman/MatrixGraph.cpp
+++ TravelingSalesman/MatrixGraph.cpp
@@ -1,2 +1,38 @@
 #include "MatrixGraph.h"
 
+using namespace std;
+
+MatrixGraph::MatrixGraph(unsigned num_nodes)
+{
+
+}
+
+MatrixGraph::~MatrixGraph()
+{
+
+}
+
+int MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight)
+{
+	return 0;
+}
+
+EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const
+{
+	return NULL;
+}
+
+unsigned MatrixGraph::degree(NodeID u) const
+{
+	return 0;
+}
+
+unsigned MatrixGraph::size() const
+{
+	return 0;
+}
+
+unsigned MatrixGraph::numEdges() const
+{
+	return 0;
+}
