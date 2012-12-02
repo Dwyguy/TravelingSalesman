f825cca9b0c2ee626866d2e402d8f87277d4f713
Matthew Dwyer <dwyermw@muohio.edu>
2012-12-01 19:25:01 -0500

    getAdj and weight are written

--- TravelingSalesman/MatrixGraph.cpp
+++ TravelingSalesman/MatrixGraph.cpp
@@ -6,9 +6,11 @@ MatrixGraph::MatrixGraph(unsigned num_nodes)
 {
 	M.resize(num_nodes);
 	for(int x = 0; x < num_nodes; x++)
+	{
 		M[x].resize(num_nodes);
-
-
+		for(int y = 0; y < num_nodes; y++)
+			M[x][y] = 0;
+	}
 
 	num_edges = 0;
 }
@@ -20,24 +22,32 @@ MatrixGraph::~MatrixGraph()
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight)
 {
-	/*
 	if(u != v)
-	M[u][v] = weight;
-	M[v][u] = weight;
+	{
+		M[u][v] = weight;
+		M[v][u] = weight;
+	}
 	else
-	M[u][v] = -1;
-
-	*/
+		M[u][v] = -1;
 }
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const
 {
-	/*
 	if(M[u][v] != -1)
-	return M[u][v];
+		return M[u][v];
 	else
-	return 0;
-	*/
+		return 0;
+}
+
+std::list<NWPair> MatrixGraph::getAdj(NodeID u) const
+{
+	list<NWPair>* nodeList = new std::list<NWPair>();
+
+	for(int x = 0; x < M.size(); x++)
+		if(M[u][x] != 0)
+			nodeList->push_back(make_pair(u, M[u][x]));
+
+	return *nodeList;
 }
 
 unsigned MatrixGraph::degree(NodeID u) const
